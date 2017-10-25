8c6acc12963228d7fa4a9e6f1f8ecba12aa7d243
Stephany Choi <s.choi417@gmail.com>
2017-10-25 15:16:56 +0000

    Add get me to retrieve current user

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,4 +12,8 @@ class Kele
         @auth_token = response["auth_token"]
     end
     
+    def get_me
+        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
 end
